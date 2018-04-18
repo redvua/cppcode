65008e64fa74021270ca99b0c61c0778cd591646
Ed B <redvepr@gmail.com>
2018-04-18 10:02:33 -0400

    ver.5

--- index.cpp
+++ index.cpp
@@ -3,7 +3,8 @@ using namespace std;
 // comment 
 
 int main() {
- cout << "ver.4" << endl;
- cout << "Hello world!!!!\n";
+ cout << "ver.5" << endl;
+ cout << "--------" << endl;
+ cout << "Hello world!\n";
  return 0;
 }
